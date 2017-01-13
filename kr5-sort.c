5fc9cc8d28e3999a942287adc1c9533f0273ff2e
Evans <Shoowa@users.noreply.github.com>
2017-01-12 23:57:00 -0800

    draft sorting program that uses pointers

---
+++ kr5-sort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+void qsort(char *lineptr[], int left, int right);
+
+main(int argc, char *argv[])
+{
+}
+
+#define MAXLEN 1000
+int getline(char *, int);
+char *alloc(int);
+
+int readlines(char *lineptr[], int maxlines)
+{
+}
+
+void writlines(char *lineptr[], int nlines)
+{
+}
